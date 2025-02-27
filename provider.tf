3a3f23d4261b50c4bdb4ebf2a92934e7761e50c9
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-02-27 14:38:06 -0600

    updated provider.tf file

--- provider.tf
+++ provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.73.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
+      version = "~> 5.0"
     }
   }
 
